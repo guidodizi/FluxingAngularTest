a6df648abd00edeceb39d22400ce62d5bf0cb7cd
Guido Dizioli <guidodizi@gmail.com>
2018-05-21 13:34:39 -0300

    preview tag

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
-import { AppRoutingModule } from './core/app-routing/app-routing.module'
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { AppRoutingModule } from "./core/app-routing/app-routing.module";
+import { StoreModule } from "@ngrx/store";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 
-import { petTagReducer } from './core/reducers/pet-tag.reducer'
-import { AppComponent } from './core/app.component';
-import { HomeComponent } from './core/pages/home/home.component';
-import { CreateComponent } from './core/pages/create/create.component';
-import { CompleteComponent } from './core/pages/complete/complete.component';
-import { TagShapeComponent } from './core/pages/create/tag-shape/tag-shape.component';
-import { TagTextComponent } from './core/pages/create/tag-text/tag-text.component';
-import { TagExtrasComponent } from './core/pages/create/tag-extras/tag-extras.component';
-import { TagPreviewComponent } from './tag-preview/tag-preview.component';
+import { petTagReducer } from "./core/reducers/pet-tag.reducer";
+import { AppComponent } from "./core/app.component";
+import { HomeComponent } from "./core/pages/home/home.component";
+import { CreateComponent } from "./core/pages/create/create.component";
+import { CompleteComponent } from "./core/pages/complete/complete.component";
+import { TagShapeComponent } from "./core/pages/create/tag-shape/tag-shape.component";
+import { TagTextComponent } from "./core/pages/create/tag-text/tag-text.component";
+import { TagExtrasComponent } from "./core/pages/create/tag-extras/tag-extras.component";
+import { TagPreviewComponent } from "./core/pages/tag-preview/tag-preview.component";
 
 @NgModule({
   declarations: [
@@ -32,10 +32,10 @@ import { TagPreviewComponent } from './tag-preview/tag-preview.component';
     FormsModule,
     StoreModule.forRoot({ petTag: petTagReducer }),
     StoreDevtoolsModule.instrument({
-      maxAge: 25, // Retains last 25 states
+      maxAge: 25 // Retains last 25 states
     })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
